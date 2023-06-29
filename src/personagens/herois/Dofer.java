49d961489b7fc5f8b024868d79c8bd15e9ea5919
RenAlv08 <renan.c.alves@edu.unirio.br>
2023-06-29 20:28:10 -0300

    Histórias atualizadas

--- src/personagens/herois/Dofer.java
+++ src/personagens/herois/Dofer.java
@@ -12,33 +12,26 @@ public final class Dofer extends Heroi {
     @Override
     public void contarHistoria() {
         System.out.print("""
-                Respeitado cavaleiro da Guarda Real de Javacity, Dofer,
-                 estava mais um dia na guarda das muralhas da cidade quando, com um som ensurdecedor,
-                 uma luz verde em forma de garra, rapidamente se materializando, emana das sombras e mata
-                 seu irmão e parceiro de armas Mikelos. Ao vê-lo Dofer utiliza seu famoso ataque espada flamejante
-                 e parte para cima da garra que, para sua surpresa, superou sua extrema velocidade e ataca-o
-                 antes que pudesse sequer arranhá-la, o cavaleiro caiu paralisado e, fechando os olhos,
-                 viu por alguns instantes a garra voltando para as sombras e espairecer lentamente.
-                 Semanas após o ocorrido, Dofer encontra-se no hospital e, ao abrir seus olhos, começa
-                 a sentir uma dor latente em seu peito, ele olha para baixo e vê um buraco onde seu coração
-                 deveria estar. Dofer gritou e gritou até que o médico de plantão chega tentando acalmá-lo e
-                 explicá-lo um pouco mais sobre o seu estado ao chegar no hospital.
-                 O médico contou que em seu peito havia um buraco de aproximadamente um palmo de largura,
-                 sob intensas hemorragias, seu coração estava batendo exposto, quase como se quisesse
-                 desprender-se do corpo do cavaleiro. Além disso, em seu coração havia uma substância verde,
-                 acídica, corroendo sua carne, que o doutor nunca vira antes, mas que suspeitava ser algum tipo
-                 de magia negra. Infelizmente, a única solução para salvá-lo foi retirar seu coração infectado e
-                 realizar um tratamento intensivo de materialização de mana — energia mágica que todos os seres
-                 humanos compartilham, ainda que em níveis diferentes, e que pode ser transferida para outro corpo —,
-                 com os magos renomados da cidade tentando um procedimento arriscado de criar um proto-coração com
-                 sua já existente mana e com a energia doada pelos humildes habitantes de Javacity,
-                 mas apesar do êxito no tratamento, o médico o avisou que ele nunca mais teria o mesmo vigor,
-                 as mesmas forças e a mesma mana em seu corpo que antes.
-                 Desde esse dia, Dofer treinou ininterruptamente por dois anos, em dias quentes e noites chuvosas,
-                 ultrapassando a fadiga de cada músculo em seu corpo para partir em busca do assassino de seu irmão.
-                 Após cinco anos de uma jornada incansável por respostas, o cavaleiro ouviu boatos de que ao Sul de Javacity,
-                 nos subterrâneos da Floresta Sombria de Sonfejer habitava um mago negro que realizava experimentos que
-                 emanavam longínquas e estranhas luzes verdes, o herói sentiu sua espinha arrepiar, o cavaleiro já sabia o que fazer.""");
+                Respeitado coronel da Guarda Real de Java City e estrategista do rei, Dofer, estava mais um dia na guarda das muralhas
+                fazendo sua patrulha diária quando, com um som ensurdecedor, uma luz verde em forma de garra, rapidamente se materializando,
+                emana das sombras e mata seu irmão e parceiro de armas Mikelos. Ao vê-lo Dofer utiliza seu famoso ataque espada flamejante e
+                parte para cima da garra que, para sua surpresa, superou sua extrema velocidade e ataca-o antes que pudesse sequer arranhá-la,
+                o cavaleiro foi ao chão e, fechando os olhos, viu por alguns instantes a garra voltando para as sombras e espairecer lentamente.
+                Semanas após o ocorrido, Dofer encontra-se no hospital e, ao abrir seus olhos, começa a sentir uma dor latente em seu peito, ele
+                olha para baixo e vê um buraco onde seu coração deveria estar. Dofer gritou e gritou até que o médico de plantão chega tentando
+                acalmá-lo e explicá-lo um pouco mais sobre o seu estado ao chegar no hospital. O médico contou que em seu peito havia um buraco de
+                aproximadamente um palmo de largura, sob intensas hemorragias, seu coração estava batendo exposto, quase como se quisesse desprender-se
+                do corpo do cavaleiro. Além disso, em seu coração havia uma substância verde, acídica, corroendo sua carne, que o doutor nunca
+                vira antes, mas que suspeitava ser algum tipo de magia negra. Infelizmente, a única solução para salvá-lo foi retirar seu coração
+                infectado e realizar um tratamento intensivo de materialização de mana — energia mágica que todos os seres humanos compartilham,
+                ainda que em níveis diferentes, e que pode ser transferida para outro corpo —, com os magos renomados da cidade tentando um procedimento
+                arriscado de criar um proto-coração com sua já existente mana e com a energia doada pelos humildes habitantes de Javacity, mas apesar
+                do êxito no tratamento, o médico o avisou que ele nunca mais teria o mesmo vigor, as mesmas forças e a mesma mana em seu corpo que antes.
+                Desde esse dia, Dofer treinou ininterruptamente por dois anos, em dias quentes e noites chuvosas, ultrapassando a fadiga de cada
+                músculo em seu corpo para partir em busca do assassino de seu irmão. Após cinco anos de uma jornada incansável por respostas,
+                o cavaleiro, ao retornar para Java City ouviu boatos de que ao Sul da cidade, nos subterrâneos da Floresta Sombria de Sonfejer habitava
+                um mago negro que realizava experimentos que emanavam longínquas e estranhas luzes verdes, o herói sentiu sua espinha arrepiar,
+                ele sabia o que tinha que fazer.""");
     }
     @Override
     public void poderEspecial1(Vilao vilao) {
--- src/personagens/herois/Rodan.java
+++ src/personagens/herois/Rodan.java
@@ -15,26 +15,28 @@ public final class Rodan extends Heroi {
     public void contarHistoria() {
         System.out.print("""
                 Nascido em Java City, Rodan abandonou uma infância comum para treinar as artes mágicas em Arquinnus,
-                famosa cidade dos magos à Leste de Java City, após ser encontrado pelo renomado mago-mestre Alighieri
-                criando pequenas bolas de energia próximo aos limites da cidade. Mostrando talento e sabedoria,
-                o jovem começava a se tornar tão famoso quanto seu mestre e, assim, ganha de Alighieri a permissão para desvencilhar-se de seus caminhos de aprendiz.\s
+                famosa cidade dos magos à Leste de Java City, após ser encontrado pelo renomado mago-mestre Alighieri criando pequenas
+                bolas de energia próximo aos limites da cidade. Mostrando talento e sabedoria, o jovem começava a se tornar tão
+                famoso quanto seu mestre e, assim, ganha de Alighieri a permissão para desvencilhar-se de seus caminhos de aprendiz.\s
                 Quinze anos depois, lutando na famosa Batalha de Latibrurves, o mago recebeu, no lugar do príncipe, uma flecha envenenada
                 com uma substância verde em seu abdômen. Quase aceitando seu destino, ele foi salvo graças a uma jovem médica chamada Júliper que,
                 vendo distante o ocorrido, fora rápida o suficiente para retirar a flecha antes que o veneno se alastrasse pelo corpo.
-                Rodan apaixona-se por sua salvadora e casa-se com a moça após ganhar seu coração. Após o acontecimento chegar nos ouvidos do rei de Java City,
-                Rodan tornou-se o mago-mestre conselheiro do rei.\s
-                No ano seguinte, arrumando sua biblioteca, o mago encontra um bilhete em sua mesa que, instantes atrás, não estava ali.
-                Em uma língua antiga, que quase não pôde decifrar, estava escrito: “A flecha não foi atirada em vão”, imediatamente correu para sua casa e
-                encontrou Júliper petrificada, felizmente, com vida. Rodan entendera tudo, o emissário da flecha sabia que se atirasse nele ele conseguiria desviar,
-                mas mudando o alvo para o príncipe o mago colocaria-se na frente da flecha, era ele quem deveria estar paralisado. Não sabendo o que fazer,
-                ele foi de encontro ao seu antigo mestre Alighieri em busca de respostas. Alighieri o disse que antes de Rodan, ele tivera outro pupilo,
-                seu nome era Napda. Por anos, Napda fora seu único aprendiz. Num dia, entretanto, quando viu que seu mestre decidiu ensinar outro jovem,
-                seu coração nublou-se de inveja. Napda nunca mais fora o mesmo. Alighieri dormia tranquilo uma noite quando, acordado por uma estranha
-                luz verde que saía do quarto de seu novo aprendiz, encontrou o jovem paralisado. Napda, depois disso, nunca mais fora visto e
-                Alighieri deixou o outro rapaz aos cuidados do hospital de Arquinnus e, depois de dez meses tentando curá-lo, seu coração súbitamente parou.
-                Rodan sabia que era Napda quem ele deveria  encontrar, o primeiro aprendiz de seu antigo mestre, agora, um mago negro.
-                Por nove meses Rodan procurou por Napda, havia pouco tempo. Ao acampar próximo à Floresta Sombria de Sonfejer,
-                o mago viu uma fraca e estranha luz verde no fundo da mata, ele sabia que a salvação de sua amada estava próxima.
+                Rodan apaixona-se por sua salvadora e casa-se com a moça após ganhar seu coração. Após o acontecimento chegar nos ouvidos do rei
+                de Java City, Rodan tornou-se o mago-mestre conselheiro do rei.\s
+                No ano seguinte, arrumando sua biblioteca, o mago encontra um bilhete em sua mesa que, instantes atrás,
+                não estava ali. Em uma língua antiga, que quase não pôde decifrar, estava escrito: “A flecha não foi atirada em vão”,
+                imediatamente correu para sua casa e encontrou Júliper petrificada, felizmente, com vida. Rodan entendera tudo,
+                o emissário da flecha sabia que se atirasse nele ele conseguiria desviar, mas mudando o alvo para o príncipe o mago colocaria-se na frente
+                da flecha, era ele quem deveria estar paralisado. Não sabendo o que fazer, ele foi de encontro ao seu antigo mestre Alighieri
+                em busca de respostas. Alighieri o disse que antes de Rodan, ele tivera outro pupilo, seu nome era Napda. Por anos,
+                Napda fora seu único aprendiz. Num dia, entretanto, quando viu que seu mestre decidiu ensinar outro jovem, seu coração nublou-se de inveja.
+                Napda nunca mais fora o mesmo. Alighieri dormia tranquilo uma noite quando, acordado por uma estranha luz verde que saía do quarto de seu
+                novo aprendiz, encontrou o jovem paralisado. Napda, depois disso, nunca mais fora visto e Alighieri deixou o outro rapaz aos cuidados do
+                hospital de Arquinnus e, depois de dez meses tentando curá-lo, seu coração súbitamente parou. Rodan sabia que era Napda quem ele deveria
+                encontrar, o primeiro aprendiz de seu antigo mestre, agora, um mago negro.\s
+                Por nove meses Rodan procurou por Napda, havia pouco tempo. Ele volta para Java City para tentar reunir informações e ver o estado de Júliper e,
+                para sua surpresa, ouve boatos de que, na Floresta Sombria de Sonfejer, foram vistas estranhas luzes verdes no fundo da mata, ele sabia
+                que a salvação de sua amada finalmente estava próxima.                                                                                                            \s
                 """);
     }
 
