e7ed4d25d3b5455fcd75f318aab8f34cbb09d836
Fernando Gama <ogfernandog@gmail.com>
2023-07-09 09:36:50 -0300

    Colocando os atributos em comum da classe Personagem

--- src/personagens/viloes/Vilao.java
+++ src/personagens/viloes/Vilao.java
@@ -1,11 +1,12 @@
 package personagens.viloes;
 
+import personagens.AtaquePadrao;
 import personagens.Personagem;
 import personagens.PoderEspecial;
 
 public abstract class Vilao extends Personagem implements Obstaculo {
-    public Vilao(String nome, Integer vida, Integer mana, Integer forcaAtaque, PoderEspecial poderEspecial1) {
-        super(nome, vida, mana, forcaAtaque, poderEspecial1);
+    public Vilao(String nome, Integer vida, Integer mana, AtaquePadrao ataquePadrao, PoderEspecial poderEspecial1) {
+        super(nome, vida, mana, ataquePadrao, poderEspecial1);
     }
 
     @Override
