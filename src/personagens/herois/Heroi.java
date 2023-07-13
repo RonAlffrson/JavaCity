b68a75475d89f4fcbfa8d67d8602e11c393078d6
RenAlv08 <renan.c.alves@edu.unirio.br>
2023-07-13 13:04:01 -0300

    Erro customizado 'ErroOpcaoHeroiException' criado

--- src/personagens/herois/Heroi.java
+++ src/personagens/herois/Heroi.java
@@ -23,9 +23,9 @@ public abstract class Heroi extends Personagem implements AcoesHerois {
         this.poderEspecial2 = poderEspecial2;
     }
 
-    public static void verificarOpcaoHeroi(int opcaoHeroi) throws IllegalArgumentException{
+    public static void verificarOpcaoHeroi(int opcaoHeroi) throws ErroOpcaoHeroiException{
         if(opcaoHeroi != 1 && opcaoHeroi != 2){
-            throw new IllegalArgumentException("Erro: Digite 1 ou 2 para escolher o heroi");
+            throw new ErroOpcaoHeroiException("Erro: Digite 1 ou 2 para escolher o heroi");
         }
     }
 
@@ -63,7 +63,6 @@ public abstract class Heroi extends Personagem implements AcoesHerois {
     public void aumentarVidaRodada(){
         aumentarVida(3);
     }
-
 
 
     @Override
