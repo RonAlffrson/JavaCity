6ccf2960f9a0f475e3fcbadad80118e9ece11cda
RenAlv08 <renan.c.alves@edu.unirio.br>
2023-07-15 10:54:13 -0300

    retirado Scanner, não está sendo usado

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         Heroi jogador;
 
         Luta.mensagemInicio();
