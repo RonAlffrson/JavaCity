ff26f0fec472118d3789be0a097a4b4a67e6fa0a
RenAlv08 <renan.c.alves@edu.unirio.br>
2023-07-14 21:37:24 -0300

    método batalhar implementado validador de opcaoJogador mudado

--- src/Luta.java
+++ src/Luta.java
@@ -11,6 +11,7 @@ public class Luta {
     public Luta(Heroi heroi, Vilao vilao){
         this.heroi = heroi;
         this.vilao = vilao;
+        //todo - método batalhar no construtor
 
     }
     public static void lancarExcecaoOpcaoAtaque(int opcaoAtaque) throws ErroOpcaoAtaqueException{
@@ -18,6 +19,7 @@ public class Luta {
             throw new ErroOpcaoAtaqueException("Erro : digite um valor entre 1 e 4");
         }
     }
+    //todo - não faz sentido deixar o método mensagemInicio() nessa classe
     public static void mensagemInicio(){
         System.out.println("Bem-vindo a JavaCity");
         System.out.println("=--=-=-=-=-=-=-=-=-=-=-=-=-=-=");
@@ -54,53 +56,68 @@ public class Luta {
         } while (!validador);
         return opcaoHeroi;
     }
-    public  void batalhar(){
-        System.out.println("Batalha:\n" + heroi.getNome() +" VS " + vilao.getNome());
+    public void batalhar(){
         boolean validador = false;
-        int opcaoJogaodor = 0;
-        while(heroi.getVida() != 0 && vilao.getVida() != 0) {
-            System.out.println(heroi.getNome() + "\nVida: " + heroi.getVida() + " / " + heroi.getMax_vida() +
-                    "\nMana:" + heroi.getMana() + " / " + heroi.getMAX_MANA() + "\n");
+        int opcaoJogaodor;
 
-            System.out.println(vilao.getNome() + "\nVida: " + vilao.getVida() + "\n");
+        System.out.println("Batalha:\n" + heroi.getNome() +" VS " + vilao.getNome());
+
+        while(heroi.getVida() != 0 && vilao.getVida() != 0) {
+            System.out.println("\n" + heroi.getNome() + "\n-Vida: " + heroi.getVida() + " / " + heroi.getMax_vida() +
+                    "\n-Mana: " + heroi.getMana() + " / " + heroi.getMAX_MANA() + "\n");
+
+            System.out.println(vilao.getNome() + "\n-Vida: " + vilao.getVida() + "\n");
 
            opcaoJogaodor = opcaoJogador();
 
-            if(opcaoJogaodor == 1) {
+            if(opcaoJogaodor == 1){
                 int opcaoAtaque = opcaoAtaque();
+
+                switch (opcaoAtaque){
+                    case 1 -> heroi.ataquePadrao(vilao);
+                    case 2 -> heroi.poderEspecial1(vilao);
+                    case 3 -> heroi.poderEspecial2();
+                }
             }
-
-
             else if(opcaoJogaodor == 2){
                 System.out.println("Item que você deseja usar:");
             }
 
+            vilao.decidirAtaque(heroi);
+            //ultima coisa a fazer dentro desse while:
+            heroi.aumentarVidaRodada();
+            heroi.aumentarManaRodada();
+            vilao.sortearPocao(heroi);
+            contadorRodadas++;
 
-        } // to pensando em criar o metodo opcoes jogaodr que retornar esse metodo
+        } // to pensando em criar o metodo opcoes jogaodr que retorna esse metodo
+
     }
     private static int opcaoJogador(){
         Scanner input = new Scanner(System.in);
         int opcaoJogaodor = 0;
-        boolean validador = false;
+        boolean validador;
         do {
             try {
-                validador = false;
+                validador = true;
+
                 System.out.println("Opções do jogador:\n");
                 System.out.println("(1) Ataques");
                 System.out.println("(2) Iventario");
                 System.out.println("Digite a opção desejada: ");
+
                 opcaoJogaodor = input.nextInt();
                 Heroi.verificarOpcaoHeroi(opcaoJogaodor); // esse metodo lanca uma exceçãp
             } catch (InputMismatchException e) {
                 input.nextLine();
-                validador = true;
+                validador = false;
                 System.out.println("Erro : Digite 1 ou 2 para escolher a opção desejada\n");
             } catch (Exception e) {
                 input.nextLine();
                 System.out.println("Erro desconhecido: digite um valor valido");
-                validador = true;
+                validador = false;
             }
-        }while (validador); // fim do while validador
+        }while (!validador); // fim do while validador
 
         return  opcaoJogaodor;
     }
